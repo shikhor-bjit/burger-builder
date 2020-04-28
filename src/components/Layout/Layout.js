157e0660a7cec5a062d499464f90af9a30dba8c9
Shikhor Kumer Roy <shikhor.kumer@bjitgroup.com>
2020-04-29 00:17:47 +0600

    Open and Close Action to SideDrawer

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -4,14 +4,24 @@ import BurgerBuilder from "../../containers/BurgerBuilder/BurgerBuilder";
 import Toolbar from "../Navigation/Toolbar/Toolbar";
 import SideDrawer from "../Navigation/SideDrawer/SideDrawer";
 
-const Layout = () => {
-    return (
-        <div className="Layout">
-            <Toolbar/>
-            <SideDrawer/>
-            <BurgerBuilder/>
-        </div>
-    );
+class Layout extends React.Component {
+    state = {
+        isSideDrawerOpen: true
+    }
+
+    closeSideDrawer = () => {
+        this.setState({isSideDrawerOpen: false});
+    }
+
+    render() {
+        return (
+            <div className="Layout">
+                <Toolbar/>
+                <SideDrawer open={this.state.isSideDrawerOpen} clicked={this.closeSideDrawer}/>
+                <BurgerBuilder/>
+            </div>
+        );
+    }
 }
 
 export default Layout;
--- src/components/Navigation/SideDrawer/SideDrawer.js
+++ src/components/Navigation/SideDrawer/SideDrawer.js
@@ -1,14 +1,23 @@
 import React from 'react';
 import './SideDrawer.css';
+import Aux from "../../hoc/Aux";
 import Logo from "../../Logo/Logo";
 import NavigationItems from "../NavigationItems/NavigationItems";
+import Backdrop from "../../UI/Backdrop/Backdrop";
 
 const SideDrawer = props => {
+    let attachedClasses = ['SideDrawer Close'];
+    if (props.open) {
+        attachedClasses = ['SideDrawer Open'];
+    }
     return (
-        <div className={'SideDrawer'}>
-            <Logo height={'15%'}/>
-            <NavigationItems/>
-        </div>
+        <Aux>
+            <Backdrop show={props.open} clicked={props.clicked}/>
+            <div className={attachedClasses.join(' ')}>
+                <Logo height={'15%'}/>
+                <NavigationItems/>
+            </div>
+        </Aux>
     );
 };
 
