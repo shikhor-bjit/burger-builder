35dcb4df98ff608772b6f28fcb4389642a3c0ca3
Shikhor Kumer Roy <shikhor.kumer@bjitgroup.com>
2020-04-28 17:57:38 +0600

    Updates

---
+++ precache-manifest.c14ff18561d51a221ba35747cf6ddbf5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a92a54094f8110aeb284e1ae936245d1",
+    "url": "/burger-builder/index.html"
+  },
+  {
+    "revision": "5f35428df5d010bec4d8",
+    "url": "/burger-builder/static/css/main.ae6cc93d.chunk.css"
+  },
+  {
+    "revision": "2d658ba9fe024c485070",
+    "url": "/burger-builder/static/js/2.9ff3a6c7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/burger-builder/static/js/2.9ff3a6c7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5f35428df5d010bec4d8",
+    "url": "/burger-builder/static/js/main.d29ffdcb.chunk.js"
+  },
+  {
+    "revision": "dfa5a77a801fc3fc369b",
+    "url": "/burger-builder/static/js/runtime-main.3f8c4ff2.js"
+  }
+]);
