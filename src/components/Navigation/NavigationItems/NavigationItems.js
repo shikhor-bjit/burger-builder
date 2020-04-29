345d2362ef4df87f1816e73a53dcdb538eb1b226
Shikhor Kumer Roy <shikhor.kumer@bjitgroup.com>
2020-04-29 08:15:21 +0600

    Fix Navigation URL

--- src/components/Navigation/NavigationItems/NavigationItems.js
+++ src/components/Navigation/NavigationItems/NavigationItems.js
@@ -5,8 +5,8 @@ import NavigationItem from "./NavigationItem/NavigationItem";
 const NavigationItems = props => {
     return (
         <ul className={'NavigationItems'}>
-            <NavigationItem link={'/'} active={true}>{'Burger Builder'}</NavigationItem>
-            <NavigationItem link={'/'}>{'Checkout'}</NavigationItem>
+            <NavigationItem link={'/burger-builder'} active={true}>{'Burger Builder'}</NavigationItem>
+            <NavigationItem link={'/burger-builder'}>{'Place Order'}</NavigationItem>
         </ul>
     );
 };
