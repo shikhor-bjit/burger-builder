55c52143104f398588ad1656fde4bd962d3e54c8
Shikhor Kumer Roy <shikhor.kumer@bjitgroup.com>
2020-05-05 13:16:09 +0600

    Add react-router, react-router-dom dependencies

---
+++ src/containers/Auth/Auth.js
@@ -0,0 +1,19 @@
+import React, {Component} from 'react';
+
+class Auth extends Component {
+    state = {}
+
+    render() {
+        return (
+            <div className={'Auth'}>
+                <form>
+
+                </form>
+            </div>
+        );
+    }
+}
+
+Auth.propTypes = {};
+
+export default Auth;
